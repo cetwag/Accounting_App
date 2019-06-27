598087666e08f635cdcb9faffada2005f11bf162
cetwaggit <627137962@qq.com>
2019-06-27 10:18:57 +0800

    新建java包，增加可维护性

--- app/src/main/java/com/example/accounting_app/activity/MainActivity.java
+++ app/src/main/java/com/example/accounting_app/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.accounting_app;
+package com.example.accounting_app.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.accounting_app.R;
 
 public class MainActivity extends AppCompatActivity {
 
